cd49a3419ce0b0bf7f4c82870ace2bb689bd891b
Andrew-Nzioki <andrewkyalonzioki001@gmail.com>
2023-01-17 00:30:33 +0300

    finally I can add documents

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,24 +1,26 @@
-const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017/playground')
+const mongoose= require('mongoose')
+const {Schema}=mongoose;
+
+mongoose.connect('mongodb://127.0.0.1:27017/playground')
     .then(()=>console.log('connected to MongoDB'))
     .catch(err=>console.error('could not connect to MongoDB ...',err))
 
-const courseSchema= new mongoose.Schema({
+const courseSchema= new Schema({
     name:String,
     author:String,
     tags:[String],
     date:{type:Date,default:Date.now},
     isPublished:Boolean
-})
+});
 const Course=mongoose.model('Course',courseSchema);
 
 async function createCourse(){
 
     const course= new Course({
-        name:'Node js course',
-        author:'Andrew',
+        name:' js course',
+        author:'Chuwa',
         tags:['node','backend'],
-        isPublished:true
+        isPublished:false
  });
  const result=await course.save();
  console.log(result)
