95db4aa156d7b628f7082144d7cda00440844cea
“Agnes-Nzioki” <agnesnzioki001@gmail.com>
2023-01-26 20:46:14 +0300

    add

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,52 +1,34 @@
-const mongoose= require('mongoose')
-const {Schema}=mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-mongoose.connect('mongodb://127.0.0.1:27017/playground')
-    .then(()=>console.log('connected to MongoDB'))
-    .catch(err=>console.error('could not connect to MongoDB ...',err))
-
+mongoose.connect('mongodb://127.0.0.1:27017/playground');
 const courseSchema = new Schema({
   name: String,
   author: String,
-  media: String,
-  date: { type: Date, default: Date.now },
+  tags: [String],
+  date: Date,
   isPublished: Boolean,
 });
 const Course = mongoose.model('Course', courseSchema);
 
-async function createCourse() {
-  const course = new Course({
-    name: ' Angular frontend',
-    author: 'Himo',
-    media: 'CNN',
-    tags: ['node', 'backend'],
-    isPublished: false,
-  });
-  const result = await course.save();
-  console.log(result);
-}
-async function getCourses() {
-  const pageNumber = 2;
-  const pageSize = 10;
-  const courses = await Course.find({ author: 'Giddy', isPublished: true })
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
-    .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
-  console.log(courses);
-}
+// async function getCourses() {
+//   const pageNumber = 2;
+//   const pageSize = 10;
+//   const courses = await Course.find({ author: 'Giddy', isPublished: true })
+//     .skip((pageNumber - 1) * pageSize)
+//     .limit(pageSize)
+//     .sort({ name: 1 })
+//     .select({ name: 1, tags: 1 });
+//   console.log(courses);
+// }
+
 async function updateCourse(id) {
-  //Approach: Query first
-  //findById()
-  //modify its properties
-  //save
   const course = await Course.findById(id);
   if (!course) return;
   course.isPublished = true;
-  course.author = 'Another Author';
+  course.author = 'Another one';
+  course._id = id;
   const result = await course.save();
   console.log(result);
-  //Approach: Update first
 }
-//getCourses();
 updateCourse('5a68fdc3615eda645bc6bdec');
